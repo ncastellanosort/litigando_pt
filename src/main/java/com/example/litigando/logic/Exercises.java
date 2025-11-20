13809269ce9dcc2c6317f50cdcea292c6a9533cd
ncastellanosort <nicolasfelipe00234@gmail.com>
2025-11-20 10:32:54 -0500

    chore: logic methods

---
+++ src/main/java/com/example/litigando/logic/Exercises.java
@@ -0,0 +1,12 @@
+package com.example.litigando.logic;
+
+public class Exercises {
+  public static void FrequencyNumbers(int[] numbers) {
+  }
+
+  public static void ObjectiveSum(int[] numbers, int target) {
+  }
+
+  public static void ParenthesesValidator(String text) {
+  }
+}
