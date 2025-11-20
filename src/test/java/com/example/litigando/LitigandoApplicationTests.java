8219a3262eb6969f8537b6da3e69c9a245ca7dff
ncastellanosort <nicolasfelipe00234@gmail.com>
2025-11-20 10:13:25 -0500

    files

---
+++ src/test/java/com/example/litigando/LitigandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.litigando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitigandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
