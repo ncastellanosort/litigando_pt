6c7a99330d00e36cdb0f79a84a0e7c20de18b69f
ncastellanosort <nicolasfelipe00234@gmail.com>
2025-11-21 03:55:52 -0500

    feat: added save user with DTO data

--- src/main/java/com/example/litigando/csv/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/litigando/csv/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.example.litigando.csv.model.User;
+import com.example.litigando.csv.model.UserDTO;
 import com.example.litigando.csv.repository.UserRepository;
 import com.example.litigando.csv.service.UserService;
 
@@ -20,5 +21,17 @@ public class UserServiceImpl implements UserService {
   @Override
   public List<User> findAll() {
     return repo.findAll();
+  }
+
+  @Override
+  public User save(UserDTO dto) {
+    User user = new User(
+        null, 
+        dto.getName(),
+        dto.getEmail(),
+        dto.getRole_id(),
+        dto.getFecha_creacion()
+    );
+    return repo.save(user);
   }
 }
