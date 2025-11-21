bb10f8223968ffbd5ecdd8fa89038a9020f28e4a
ncastellanosort <nicolasfelipe00234@gmail.com>
2025-11-21 02:33:12 -0500

    chore: created User as Entity

--- src/main/java/com/example/litigando/csv/model/User.java
+++ src/main/java/com/example/litigando/csv/model/User.java
@@ -1,7 +1,18 @@
 package com.example.litigando.csv.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "USUARIOS", schema = "SYSTEM")
 public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   public Long id;
+
   public String name;
   public String email;
   public String role;
@@ -13,5 +24,45 @@ public class User {
     this.email = email;
     this.role = role;
     this.fechaCreacion = fechaCreacion;
+  }
+
+  public Long getId() {
+	return id;
+  }
+
+  public void setId(Long id) {
+	this.id = id;
+  }
+
+  public String getName() {
+	return name;
+  }
+
+  public void setName(String name) {
+	this.name = name;
+  }
+
+  public String getEmail() {
+	return email;
+  }
+
+  public void setEmail(String email) {
+	this.email = email;
+  }
+
+  public String getRole() {
+	return role;
+  }
+
+  public void setRole(String role) {
+	this.role = role;
+  }
+
+  public String getFechaCreacion() {
+	return fechaCreacion;
+  }
+
+  public void setFechaCreacion(String fechaCreacion) {
+	this.fechaCreacion = fechaCreacion;
   }
 }
