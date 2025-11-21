3dd01b11fd9b30dde70bca83c73094d50e2ed592
ncastellanosort <nicolasfelipe00234@gmail.com>
2025-11-21 06:54:09 -0500

    feat: punto C

--- src/main/java/com/example/litigando/logic/Exercises.java
+++ src/main/java/com/example/litigando/logic/Exercises.java
@@ -9,10 +9,18 @@ import com.example.litigando.logic.dto.ParenthesesValidatorResponse;
 
 public class Exercises {
   public static Map<Integer, Integer> FrequencyNumbers(int[] numbers) {
+    // guarddar el numero y la cantidad de veces que aparece
+    // { 5: 4}
     Map<Integer, Integer> frequencyMap = new HashMap<>();
 
     // O(n), recorro el array solo 1 vez
     for (int n : numbers) {
+      /* getOrDefault, busca si n ya existe en el map
+       * si existe, devuelve el valor de n en ese momento
+       * si no existe, devuelve 0
+       *
+       * luego suma 1 al conteo, el valor que habia + 1, o 0 si no encuentra nada
+       * */
       frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
     
     }
@@ -21,16 +29,20 @@ public class Exercises {
   }
 
   public static ObjectiveSumResponse objectiveSumOptimized(int[] numbers, int target) {
+    // la clave sera el numero que ya vimos, y el valor su indice 
     Map<Integer, Integer> map = new HashMap<>(); 
 
     for (int i = 0; i < numbers.length; i++) {
       int num = numbers[i];
-      int complement = target - num;
+      int complement = target - num; // es el numero que nos hace falta para llegar al target
 
+      // verificamos si existe el complemento como key
       if (map.containsKey(complement)) {
+        // si lo encontramos devolvemos el value del complemento y el indice
         return new ObjectiveSumResponse(map.get(complement), i);
       }
 
+      // si no existe, guardamos el numero actual en el map
       map.put(num, i);
     }
 
@@ -38,11 +50,13 @@ public class Exercises {
   }
 
   public static ObjectiveSumResponse objectiveSumBrute(int[] numbers, int target) {
+    // recorre el indice principal y luego el siguiente
     for (int i = 0; i < numbers.length; i++) {
       for (int j = i + 1; j < numbers.length; j++) {
-          if (numbers[i] + numbers[j] == target) {
-              return new ObjectiveSumResponse(i, j);
-          }
+        // revisa si la suma de los 2 da el target
+        if (numbers[i] + numbers[j] == target) {
+            return new ObjectiveSumResponse(i, j);
+        }
       }
     }
 
@@ -50,23 +64,42 @@ public class Exercises {
   }
 
   public static ParenthesesValidatorResponse parenthesesValidator(String text) {
-      Stack<Character> stack = new Stack<>();
-      Map<Character, Character> pairs = Map.of(
-        ')', '(',
-        '}', '{',
-        ']', '['
-      );
+    // almacenar los parentesis abiertos
+    Stack<Character> stack = new Stack<>();
 
-      for (char c : text.toCharArray()) {
-        if (pairs.containsValue(c)) {
-          stack.push(c);
-        } else if (pairs.containsKey(c)) {
-          if (stack.isEmpty() || stack.pop() != pairs.get(c)) {
-            return new ParenthesesValidatorResponse(false);
-          }
+    /*
+     * guardar los parentesis de cierre a apertura, si encontramos uno de cierre,
+     ya sabemos cual es su apertura 
+     */
+    Map<Character, Character> pairs = Map.of(
+      ')', '(',
+      '}', '{',
+      ']', '['
+    );
+
+    // convertimos la cadena a un array de chars
+    for (char c : text.toCharArray()) {
+      // buscamos si existe un parentesis de apertura
+      if (pairs.containsValue(c)) {
+        // lo agregamos
+        stack.push(c);
+        
+        // si existe un parentesis de cierre
+      } else if (pairs.containsKey(c)) {
+        /*
+         * stack.pop() saca el ultimo elemento del stack
+         * y mira si es diferente con el parentesis de apertura esperado para el
+         * si no coinciden, estan desbalanceados
+        * */
+        if (stack.isEmpty() || stack.pop() != pairs.get(c)) {
+          /* empty es que no tiene apertura correspondiente
+            es como si entrara solo un ), el stack estaria vacio al principio
+          */
+          return new ParenthesesValidatorResponse(false);
         }
       }
+    }
 
-      return new ParenthesesValidatorResponse(stack.isEmpty());
+    return new ParenthesesValidatorResponse(stack.isEmpty());
   }
 }
